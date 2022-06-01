5b201dfa4684977d5c8af36fadfe4b0608115fcf
zero2755 <56134028+zero2755@users.noreply.github.com>
2022-06-02 00:50:26 +0900

    Add files via upload
    32bit 대신증권연동 초기버전

---
+++ src/main/java/daishin/cputil/CPE_ECT_OVERHEAT.java
@@ -0,0 +1,44 @@
+package daishin.cputil  ;
+
+import com4j.*;
+
+/**
+ */
+public enum CPE_ECT_OVERHEAT {
+  /**
+   * <p>
+   * 해당 없음
+   * </p>
+   * <p>
+   * The value of this constant is 0
+   * </p>
+   */
+  CPC_OVERHEAT_NULL, // 0
+  /**
+   * <p>
+   * 지정예고
+   * </p>
+   * <p>
+   * The value of this constant is 1
+   * </p>
+   */
+  CPC_OVERHEAT_NOTICE, // 1
+  /**
+   * <p>
+   * 지정
+   * </p>
+   * <p>
+   * The value of this constant is 2
+   * </p>
+   */
+  CPC_OVERHEAT, // 2
+  /**
+   * <p>
+   * 해제연기/연장
+   * </p>
+   * <p>
+   * The value of this constant is 3
+   * </p>
+   */
+  CPC_OVERHEAT_EXT, // 3
+}
