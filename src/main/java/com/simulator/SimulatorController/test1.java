1e550b1f94e36a51faaa2452902c12ebb1439acf
zero2755 <56134028+zero2755@users.noreply.github.com>
2022-06-07 20:17:47 +0900

    Add files via upload

---
+++ src/main/java/com/simulator/SimulatorController/test1.java
@@ -0,0 +1,24 @@
+package com.simulator.SimulatorController;
+
+import dashin.cputil.*;
+import java.util.*;
+
+public class test1 {
+	public static void main(String[] args) {
+			
+			System.out.println("���� ����");
+			
+			dashin.cputil.ICpStockCode icd =ClassFactory.createCpStockCode();
+			String strName =icd.codeToName("A056080");
+			System.out.println(strName);
+			
+			dashin.cputil.ICpCodeMgr codeMgr = ClassFactory.createCpCodeMgr();
+          
+			Object[] market = (Object[]) codeMgr.getStockListByMarket(dashin.cputil.CPE_MARKET_KIND.CPC_MARKET_KOSPI);
+			System.out.println(market.length);
+			
+			System.out.println(icd.codeToName(market[3].toString()));
+				
+	}
+ 	
+}
